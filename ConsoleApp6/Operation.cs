9c7ded01a95f32dedb91cfa2c1696de4e52b1878
Pkamgon <Pkamgon@gmail.com>
2024-01-06 10:20:27 -0700

    Add project files.

---
+++ ConsoleApp6/Operation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    //Create a class
+    public class Operation
+    {
+        //create a void method within the class
+
+        public void MathOps(int num1, int num2)
+        { 
+
+        //Perform math operation on the first integer
+
+        num1 = num1* 2;
+
+        Console.WriteLine($"The result of the operation on the first integer is:{num1}");
+        Console.WriteLine($"The second integer is {num2}");
+        Console.ReadLine();
+        }
+
+    }
+}
---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    public class Program
+    {
+         public static void Main(string[] args)
+        { 
+            //Instantiate Mathops
+            Operation mathOps = new Operation();
+            int firstInterger = 5;
+            int secondInterger = 10;
+            mathOps.MathOps(firstInterger, secondInterger);
+            mathOps.MathOps(num1: 7, num2: 15);
+        }
+
+    }
+}
